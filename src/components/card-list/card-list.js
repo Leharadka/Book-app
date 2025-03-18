4ccf78de9a629b9a90ed2c1e9fa41dd34f074a44
Aleksei <alex.porfirov@gmail.com>
2025-03-18 12:41:55 +0300

    добавил состояние загрузки

---
+++ src/components/card-list/card-list.js
@@ -0,0 +1,28 @@
+import { DivComponent } from "../../common/div-component";
+import "./card-list.css";
+
+export class CardList extends DivComponent {
+  constructor(appState, parentState) {
+    super();
+    this.appState = appState;
+    this.parentState = parentState;
+  }
+
+  search() {
+    const value = this.el.querySelector(".card_list").value;
+    this.state.searchQuery = value;
+  }
+
+  render() {
+    if (this.parentState.loading) {
+      this.el.innerHTML = `<div class="card_list__loader">Загрузка...</div>`;
+      return this.el;
+    }
+    this.el.classList.add("card_list");
+    this.el.innerHTML = `
+        <h1>Найдено книг - ${this.parentState.list.length} </h1>
+        `;
+
+    return this.el;
+  }
+}
